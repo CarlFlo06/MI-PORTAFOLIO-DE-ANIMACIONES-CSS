8ecd9ecd3a82818c0e57d794d480dd113858a269
CarlFlo06 <a.carlosfloresh@seoane.edu.pe>
2026-07-08 10:10:59 -0500

    version 10

---
+++ js/menu.js
@@ -0,0 +1,169 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+document.getElementById("menu").innerHTML = `
+
+<nav class="menu">
+    <ul class="listamenu">
+
+        <li>
+            <a href="${BASE}index.html">Inicio</a>
+        </li>
+
+        <li>
+            <a href="${BASE}ANIMACION1/ajedrez.html">Ajedrez</a>
+        </li>
+
+        <li>
+
+            <a class="subm" href="#">Animación de textos</a>
+
+            <ul class="submenu">
+
+                <li>
+                    <a href="${BASE}ANIMACION2/EJER1/ejer1.html">
+                        Manuel S. Corrales
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION2/EJER2/ejer2.html">
+                        Sam Altman
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION2/EJER3/ejer3.html">
+                        Dra. Lisa Su
+                    </a>
+                </li>
+
+            </ul>
+
+        </li>
+
+        <li>
+            <a href="${BASE}ANIMACION3/megaman.html">
+                Megaman
+            </a>
+        </li>
+
+        <li>
+
+            <a class="subm" href="#">
+                Movimiento y tiempos
+            </a>
+
+            <ul class="submenu">
+
+                <li>
+                    <a href="${BASE}ANIMACION4/nave.html">
+                        Nave espacial
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION5/abeja.html">
+                        Abeja
+                    </a>
+                </li>
+
+            </ul>
+
+        </li>
+
+        <li>
+
+            <a class="subm" href="#">
+                Animación de tarjetas
+            </a>
+
+            <ul class="submenu">
+
+                <li>
+                    <a href="${BASE}ANIMACION6/lugares.html">
+                        Turismo Perú
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION7/lenguajes.html">
+                        HTML - CSS - JS
+                    </a>
+                </li>
+
+            </ul>
+
+        </li>
+
+        <li>
+            <a href="${BASE}ANIMACION8/cocos.html">
+                Cocos
+            </a>
+        </li>
+
+        <li>
+
+            <a class="subm" href="#">
+                Animación con ClipPath
+            </a>
+
+            <ul class="submenu">
+
+                <li>
+                    <a href="${BASE}ANIMACION9/payaso.html">
+                        Payaso
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION9/oso.html">
+                        Oso
+                    </a>
+                </li>
+
+            </ul>
+
+        </li>
+
+        <li>
+
+            <a class="subm" href="#">
+                Efecto Parallax
+            </a>
+
+            <ul class="submenu">
+
+                <li>
+                    <a href="${BASE}ANIMACION10/paisaje.html">
+                        Paisaje
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION10/gimnasio.html">
+                        Afiche Gimnasio
+                    </a>
+                </li>
+
+                <li>
+                    <a href="${BASE}ANIMACION11/nombre.html">
+                        Efecto nombre
+                    </a>
+                </li>
+
+            </ul>
+
+        </li>
+
+        <li>
+            <a href="${BASE}ANIMACION12/salto.html">
+                Salto
+            </a>
+        </li>
+
+    </ul>
+</nav>
+
+`;
+
+});
